9f594ad7cc04ac87ebb7c3276cc8f5c56c47867e
Jos Pasop <jpasop@victronenergy.com>
2015-02-04 13:57:55 +0100

    Changed indentation back to TABS

--- streamcommand.py
+++ streamcommand.py
@@ -12,64 +12,64 @@ from time import sleep
 
 # Runs a command, and calls sendfeedback with the statusupdates.
 class StreamCommand(object):
-    def run(self, command, timeout, feedbacksender):
-        self.feedbacksender = feedbacksender
-        self.returncode = None
+	def run(self, command, timeout, feedbacksender):
+		self.feedbacksender = feedbacksender
+		self.returncode = None
 
-        def target():
-            logger.info('Thread started for running %s' % command)
-            self.feedbacksender.send({"status": "starting"})
+		def target():
+			logger.info('Thread started for running %s' % command)
+			self.feedbacksender.send({"status": "starting"})
 
-            try:
-                self.process = subprocess.Popen(command, stdout=subprocess.PIPE)
-            except OSError as e:
-                logger.info("Command %s could not be started, error: %s" % (command, e.strerror))
-                self.feedbacksender.send({"status": "could_not_start", "message":
-                    "Error no %s, %s" % (e.errno, e.strerror)})
+			try:
+				self.process = subprocess.Popen(command, stdout=subprocess.PIPE)
+			except OSError as e:
+				logger.info("Command %s could not be started, error: %s" % (command, e.strerror))
+				self.feedbacksender.send({"status": "could_not_start", "message":
+					"Error no %s, %s" % (e.errno, e.strerror)})
 
-                self.process = None
-                return
+				self.process = None
+				return
 
-            while self.process.poll() is None:
-                self.readandsend()
-                sleep(0.1)
+			while self.process.poll() is None:
+				self.readandsend()
+				sleep(0.1)
 
-            logger.info('Thread finished for running %s' % command)
+			logger.info('Thread finished for running %s' % command)
 
-        thread = threading.Thread(target=target)
-        thread.start()
-        thread.join(timeout)
+		thread = threading.Thread(target=target)
+		thread.start()
+		thread.join(timeout)
 
-        if self.process is None:
-            # Error message (could_not_start) has already been sendfeedback-ed: nothing left to do
-            return None
+		if self.process is None:
+			# Error message (could_not_start) has already been sendfeedback-ed: nothing left to do
+			return None
 
-        # Make sure to send all the output
-        self.readandsend()
+		# Make sure to send all the output
+		self.readandsend()
 
-        if thread.is_alive():
-            logger.info("Command %s will now be terminated because of timeout" % command)
-            self.process.terminate()  # TODO or should it be killed?
-            thread.join()
-            logger.info("Command %s has been terminated" % command)
-            self.feedbacksender.send({"status": "finished", "exitstatus": "stopped_by_timeout"})
+		if thread.is_alive():
+			logger.info("Command %s will now be terminated because of timeout" % command)
+			self.process.terminate()  # TODO or should it be killed?
+			thread.join()
+			logger.info("Command %s has been terminated" % command)
+			self.feedbacksender.send({"status": "finished", "exitstatus": "stopped_by_timeout"})
 
-        # TODO, check if the process has crashed, and give exitstatus: crashed
-        else:
-            logger.info("Command %s execution completed ok. Exitcode %d" % (command, self.process.returncode))
-            self.feedbacksender.send({"status": "finished", "exitstatus": "normal_exit",
-                 "exitcode": self.process.returncode})
+		# TODO, check if the process has crashed, and give exitstatus: crashed
+		else:
+			logger.info("Command %s execution completed ok. Exitcode %d" % (command, self.process.returncode))
+			self.feedbacksender.send({"status": "finished", "exitstatus": "normal_exit",
+				 "exitcode": self.process.returncode})
 
-        return self.process.returncode
+		return self.process.returncode
 
-    def readandsend(self):
-        # TODO: check that below code works OK with vup stdout encoding (UTF-8), including non-standard ASCII chars
+	def readandsend(self):
+		# TODO: check that below code works OK with vup stdout encoding (UTF-8), including non-standard ASCII chars
 
-        self.process.stdout.flush()
-        lines = self.process.stdout.readlines()
-        logger.debug("lines: %s" % len(lines))
-        if len(lines) > 0:
-            # Max length on pubnub is 1800 chars, and output is much better readable with the bare eye
-            # when sent per line. So no need to send it alltogether.
-            for line in lines:
-                self.feedbacksender.send({"status": "running", "xmloutput": line})
+		self.process.stdout.flush()
+		lines = self.process.stdout.readlines()
+		logger.debug("lines: %s" % len(lines))
+		if len(lines) > 0:
+			# Max length on pubnub is 1800 chars, and output is much better readable with the bare eye
+			# when sent per line. So no need to send it alltogether.
+			for line in lines:
+				self.feedbacksender.send({"status": "running", "xmloutput": line})
