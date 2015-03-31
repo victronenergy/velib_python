882118b5133ce64a0e76b366fe6f86b73d4a9155
mpvader <mvader@victronenergy.com>
2015-03-31 21:11:30 +0000

    ve_utils: added get_vrm_portal_id()

--- ve_utils.py
+++ ve_utils.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 # Use this function to make sure the code quits on an unexpected exception. Make sure to use it
 # when using gobject.idle_add and also gobject.timeout_add.
 # Without this, the code will just keep running, since gobject does not stop the mainloop on an
@@ -18,3 +17,25 @@ def exit_on_error(func, *args, **kwargs):
 		# halt when used in a dbus callback, see connection.py in the Python/Dbus libraries, line 230.
 		import os
 		os._exit(1)
+
+
+__vrm_portal_id = None
+def get_vrm_portal_id():
+	# For the CCGX, the definition of the VRM Portal ID is that it is the mac address of the onboard-
+	# ethernet port (eth0), stripped from its colons (:) and lower case.
+
+	# nice coincidence is that this also works fine when running on your (linux) development computer.
+
+	global __vrm_portal_id
+
+	if __vrm_portal_id:
+		return __vrm_portal_id
+
+	# Assume we are on linux
+	import fcntl, socket, struct
+
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	info = fcntl.ioctl(s.fileno(), 0x8927,  struct.pack('256s', 'eth0'[:15]))
+	__vrm_portal_id = ''.join(['%02x' % ord(char) for char in info[18:24]])
+
+	return __vrm_portal_id
