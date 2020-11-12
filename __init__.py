7b9891a170167599fec3203daafef95911688836
Job Feikens <job@vv32.nl>
2020-11-12 14:18:18 +0100

    Made local imports relative

--- __init__.py
+++ __init__.py
@@ -1,14 +1,6 @@
-import dbusdummyservice
-import dbusitem
-import dbusmonitor
-import gobjectwrapper
-import logger
-import mosquitto_bridge_registrator
-import settingsdevice
-import streamcommand
-import tracing
-import ve_utils
-import vedbus
+from . import dbusdummyservice, dbusitem, dbusmonitor, gobjectwrapper, logger, \
+    mosquitto_bridge_registrator, settingsdevice, streamcommand, tracing,\
+    ve_utils, vedbus
 
 __all__ = ['dbusdummyservice', 'dbusitem', 'dbusmonitor', 'gobjectwrapper',
            'logger', 'mosquitto_bridge_registrator', 'settingsdevice',
