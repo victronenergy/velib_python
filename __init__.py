32185e4b9536cc3c22a5375b6bae07992c0160e6
Job Feikens <job@vv32.nl>
2020-11-12 14:56:38 +0100

    Removed recursive import

--- __init__.py
+++ __init__.py
@@ -1,7 +1,3 @@
-from . import dbusdummyservice, dbusitem, dbusmonitor, gobjectwrapper, logger, \
-    mosquitto_bridge_registrator, settingsdevice, streamcommand, tracing,\
-    ve_utils, vedbus
-
 __all__ = ['dbusdummyservice', 'dbusitem', 'dbusmonitor', 'gobjectwrapper',
            'logger', 'mosquitto_bridge_registrator', 'settingsdevice',
            'streamcommand', 'tracing',  've_utils', 'vedbus']
