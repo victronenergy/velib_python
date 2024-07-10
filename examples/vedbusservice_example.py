fdc4e538cc0b51d69f56bad945b9ff127f613faf
Izak Burger <iburger@victronenergy.com>
2024-07-10 14:29:02 +0200

    update example

--- examples/vedbusservice_example.py
+++ examples/vedbusservice_example.py
@@ -37,9 +37,19 @@ def main(argv):
 		# Most simple and short way to add an object with an initial value of 5.
 		dbusservice.add_path('/Position', value=5)
 
-		# Most advanced wayt to add a path
+		# Most advanced way to add a path
 		dbusservice.add_path('/RPM', value=100, description='RPM setpoint', writeable=True,
 			onchangecallback=validate_new_value, gettextcallback=get_text_for_rpm)
+
+		# Many types supported
+		dbusservice.add_path('/String', 'this is a string')
+		dbusservice.add_path('/Int', 0)
+		dbusservice.add_path('/NegativeInt', -10)
+		dbusservice.add_path('/Float', 1.5)
+
+		# Call register after adding paths. More paths can be added later.
+		# This claims the service name on dbus.
+		dbusservice.register()
 
 		# You can access the paths as if the dbusservice is a dictionary
 		print('/Position value is %s' % dbusservice['/Position'])
@@ -51,11 +61,6 @@ def main(argv):
 
 		# To invalidate a value (see com.victronenergy.BusItem specs for definition of invalid), set to None
 		dbusservice['/Position'] = None
-
-		dbusservice.add_path('/String', 'this is a string')
-		dbusservice.add_path('/Int', 0)
-		dbusservice.add_path('/NegativeInt', -10)
-		dbusservice.add_path('/Float', 1.5)
 
 		print('try changing our RPM by executing the following command from a terminal\n')
 		print('dbus-send --print-reply --dest=com.victronenergy.example /RPM com.victronenergy.BusItem.SetValue int32:1200')
