e63c4a8f64cd3e1d1a74e72d25e99199c454e5fc
Izak Burger <iburger@victronenergy.com>
2024-09-17 16:17:20 +0200

    dbusmonitor: explicitly use interface on calls
    The dbus protocol allows for omitting the interface in the
    header, and then any matching method can be called. This causes
    problems with some newer python dbus libraries such as dbus-next
    and dbus-fast, and although we patched those to work correctly
    in the past, it is better to tighten this down as well.

--- dbusmonitor.py
+++ dbusmonitor.py
@@ -30,7 +30,12 @@ from functools import partial
 
 # our own packages
 from ve_utils import exit_on_error, wrap_dbus_value, unwrap_dbus_value, add_name_owner_changed_receiver
-notfound = object() # For lookups where None is a valid result
+
+# dbus interface
+VE_INTERFACE = "com.victronenergy.BusItem"
+
+# For lookups where None is a valid result
+notfound = object()
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -211,7 +216,7 @@ class DbusMonitor(object):
 		# Try to fetch everything with a GetItems, then fall back to older
 		# methods if that fails
 		try:
-			values = self.dbusConn.call_blocking(serviceName, '/', None, 'GetItems', '', [])
+			values = self.dbusConn.call_blocking(serviceName, '/', VE_INTERFACE, 'GetItems', '', [])
 		except dbus.exceptions.DBusException:
 			logger.info("GetItems failed, trying legacy methods")
 		else:
@@ -224,7 +229,7 @@ class DbusMonitor(object):
 		else:
 			try:
 				di = self.dbusConn.call_blocking(serviceName,
-					'/DeviceInstance', None, 'GetValue', '', [])
+					'/DeviceInstance', VE_INTERFACE, 'GetValue', '', [])
 			except dbus.exceptions.DBusException:
 				logger.info("       %s was skipped because it has no device instance" % serviceName)
 				return False # Skip it
@@ -238,8 +243,8 @@ class DbusMonitor(object):
 		values = {}
 		texts = {}
 		try:
-			values.update(self.dbusConn.call_blocking(serviceName, '/', None, 'GetValue', '', []))
-			texts.update(self.dbusConn.call_blocking(serviceName, '/', None, 'GetText', '', []))
+			values.update(self.dbusConn.call_blocking(serviceName, '/', VE_INTERFACE, 'GetValue', '', []))
+			texts.update(self.dbusConn.call_blocking(serviceName, '/', VE_INTERFACE, 'GetText', '', []))
 		except:
 			pass
 
@@ -255,9 +260,9 @@ class DbusMonitor(object):
 			text = texts.get(path[1:], notfound)
 			if value is notfound or text is notfound:
 				try:
-					value = self.dbusConn.call_blocking(serviceName, path, None, 'GetValue', '', [])
+					value = self.dbusConn.call_blocking(serviceName, path, VE_INTERFACE, 'GetValue', '', [])
 					service.set_seen(path)
-					text = self.dbusConn.call_blocking(serviceName, path, None, 'GetText', '', [])
+					text = self.dbusConn.call_blocking(serviceName, path, VE_INTERFACE, 'GetText', '', [])
 				except dbus.exceptions.DBusException as e:
 					if e.get_dbus_name() in (
 							'org.freedesktop.DBus.Error.ServiceUnknown',
@@ -409,7 +414,7 @@ class DbusMonitor(object):
 	# Typically seen will be sufficient and doesn't need access to the dbus.
 	def exists(self, serviceName, objectPath):
 		try:
-			self.dbusConn.call_blocking(serviceName, objectPath, None, 'GetValue', '', [])
+			self.dbusConn.call_blocking(serviceName, objectPath, VE_INTERFACE, 'GetValue', '', [])
 			return True
 		except dbus.exceptions.DBusException as e:
 			return False
@@ -441,7 +446,7 @@ class DbusMonitor(object):
 			return -1
 		# We do not catch D-Bus exceptions here, because the previous implementation did not do that either.
 		return self.dbusConn.call_blocking(serviceName, objectPath,
-				   dbus_interface='com.victronenergy.BusItem',
+				   dbus_interface=VE_INTERFACE,
 				   method='SetValue', signature=None,
 				   args=[wrap_dbus_value(value)])
 
@@ -452,7 +457,7 @@ class DbusMonitor(object):
 		if service is not None:
 			if objectPath in service.paths:
 				self.dbusConn.call_async(serviceName, objectPath,
-					dbus_interface='com.victronenergy.BusItem',
+					dbus_interface=VE_INTERFACE,
 					method='SetValue', signature=None,
 					args=[wrap_dbus_value(value)],
 					reply_handler=reply_handler, error_handler=error_handler)
